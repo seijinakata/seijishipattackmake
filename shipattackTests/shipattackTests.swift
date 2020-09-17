8fd3416e0bb338eb3871ea9f2c0d50eb0b75b26e
seijinakata <nakataseiji2001@gmail.com>
2020-09-17 12:23:45 +0900

    Initial Commit

---
+++ shipattackTests/shipattackTests.swift
@@ -0,0 +1,34 @@
+//
+//  shipattackTests.swift
+//  shipattackTests
+//
+//  Created by Owner on 6/27/1399 AP.
+//  Copyright © 1399 AP Owner. All rights reserved.
+//
+
+import XCTest
+@testable import shipattack
+
+class shipattackTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
